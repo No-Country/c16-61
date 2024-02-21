8467699bea00c035e1b23e6b32a6ecb7c7a027d5
Stephany Acosta <stephanynag@gmail.com>
2024-02-21 18:15:25 -0500

    feature: search results

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,33 @@
-import styles from './SearchBar.module.css'
-import { CustomButton } from '@/UI/button/Button'
+"use client"
 
-export function SearchBar() {
+import styles from './SearchBar.module.css';
+import { SearchResult } from "@/components/SearchResult/SearchResult";
+import React, {useState} from 'react';
+
+export function SearchBar({testArray}): JSX.Element {
+  const [showResults, setShowResults] = useState(false);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setShowResults(true); // Show search results when the button is clicked
+  };
+
+  const clearSearch = (event) => {
+    event.preventDefault();
+    setShowResults(false); // Hide search results when the button is clicked
+  };
+
   return (
     <section>
-      <form className={styles.searchForm} >
-        <input type="text" />
-        <CustomButton onClick={() => { }} text="Buscar"></CustomButton>
+      <form className={styles.searchForm}>
+        <input
+          type="text"
+          className={styles.searchInput}
+        />
+        <button className={styles.searchButton} onClick={handleSearch} type="submit">Buscar</button>
+        <button className={styles.searchButton} onClick={clearSearch} type="button">Clear</button>
       </form>
+      {showResults && <SearchResult testArray={testArray} />}
     </section>
-  )
+  );
 }
